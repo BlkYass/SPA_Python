abbc5ec8040f13bc3c2d3adb1d16fc99d2a76534
Issam Atrassi <issambni@gmail.com>
2018-12-25 02:16:32 +0100

    create databse models

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from app import db, login_manager
+from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    pref_place = db.relationship('pref_place', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.email}', '{self.pref_place}')"
+
+class pref_place(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_like = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    ln = db.Column(db.Integer, nullable=False)
+    lg = db.Column(db.Integer, nullable=False)
+    image_file = db.Column(db.LargeBinary, nullable=False, default='default.jpg')
+    user_id = db.Column(db.String(20), db.ForeignKey('user.email'), nullable=False)
+
+   # def __repr__(self):
+     #   return f"pref_place('{self.image_file', '{self.date_like}')"
