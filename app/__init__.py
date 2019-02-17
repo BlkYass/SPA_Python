9c3379734e70102e03bc0ba4e4557bd46fc6e94f
Issam Atrassi <issambni@gmail.com>
2019-02-17 22:56:09 +0100

    adding comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,28 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+#Module to intialize the application and bring together different components
 
+from flask import Flask  #import the Flask class
+from flask_sqlalchemy import SQLAlchemy  #import flask sqlAlchemy extention
+from flask_bcrypt import Bcrypt          #import flask bcrypt extention for password hasging
+from flask_login import LoginManager  
 
+#the package app is defined by the app directory and the __init__.py module  
+#and is referenced in the 'from app import routes' below
+#The app variable is defined as an instance of class Flask in the __init__.py module
+#which makes it a member of the app package.
+#here we are creating our flask application
+app = Flask(__name__) #__name__ is the name of the current Python module
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Places.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #To turn off the Flask-SQLAlchemy event system in order not to waste system resources 
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' #Setting up simple configuration for now
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Places.db' #Configuring a database named Places.db located in the main directory of the application 
+
+db = SQLAlchemy(app)   # initalize app with database
+
+bcrypt = Bcrypt(app)   # initalize app with passowrd hashing
+
+login_manager = LoginManager(app)   # initalize app with Login manager
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 
-from app import routes
+from app import routes    #in order to fix circular import we import routes module after instantiation of the app variable
