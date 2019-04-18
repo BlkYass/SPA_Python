ee565233b1d05955d2371c7b9fb45c1b5eccfaf0
Issam Atrassi <issambni@gmail.com>
2019-04-18 23:01:50 +0100

    testing without postgres

--- app/__init__.py
+++ app/__init__.py
@@ -11,16 +11,17 @@ import os
 #which makes it a member of the app package.
 #here we are creating our flask application
 app = Flask(__name__) #__name__ is the name of the current Python module
-
+'''
 if os.environ.get('ENV') == 'PRODUCTION':
     app.config['DEBUG'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
-else:
-    app.config['DEBUG'] = True
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #To turn off the Flask-SQLAlchemy event system in order not to waste system resources 
-    app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' #Setting up simple configuration for now
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Places.db' #Configuring a database named Places.db located in the main directory of the application 
+    else:
+        '''
+app.config['DEBUG'] =True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #To turn off the Flask-SQLAlchemy event system in order not to waste system resources
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' #Setting up simple configuration for now
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Places.db' #Configuring a database named Places.db located in the main directory of the application 
+        
 
 db = SQLAlchemy(app)   #initalize app with database
 
