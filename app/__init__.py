359273af845b07241ace0db5a246adce4ca0cacb
Issam Atrassi <issambni@gmail.com>
2019-04-18 19:44:26 +0100

    adding conditional logic to connect to correct db

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask  #import the Flask class
 from flask_sqlalchemy import SQLAlchemy  #import flask sqlAlchemy extention
 from flask_bcrypt import Bcrypt          #import flask bcrypt extention for password hasging
 from flask_login import LoginManager  #LoginManager class contains the code that lets the application and Flask-Login work together, such as how to load a user from an ID, where to send users when they need to log in, and the like.
-
+import os
 #the package app is defined by the app directory and the __init__.py module  
 #and is referenced in the 'from app import routes' below
 #The app variable is defined as an instance of class Flask in the __init__.py module
@@ -12,9 +12,15 @@ from flask_login import LoginManager  #LoginManager class contains the code that
 #here we are creating our flask application
 app = Flask(__name__) #__name__ is the name of the current Python module
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #To turn off the Flask-SQLAlchemy event system in order not to waste system resources 
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' #Setting up simple configuration for now
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Places.db' #Configuring a database named Places.db located in the main directory of the application 
+if os.environ.get('ENV') == 'PRODUCTION':
+    app.config['DEBUG'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+else:
+    app.config['DEBUG'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True #To turn off the Flask-SQLAlchemy event system in order not to waste system resources 
+    app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' #Setting up simple configuration for now
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Places.db' #Configuring a database named Places.db located in the main directory of the application 
 
 db = SQLAlchemy(app)   #initalize app with database
 
